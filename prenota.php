02bcc4352dfea80a6b9bec4ab3fbdd0eda9009b7
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-03-13 09:40:26 +0100

    Refactor db configuration using an external file

--- prenota.php
+++ prenota.php
@@ -1,19 +1,6 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('log_errors', '0');
-
-$host = 'localhost';
-$db = 'Tamponi';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-
-//Data Source Name
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-//Oggetto che rappresenta la connessione al database
-$pdo = new PDO($dsn, $user, $pass);
+include_once "config.php";
 
 //Variabili valorizzate tramite POST
 $codice_fiscale = $_POST['codice'];

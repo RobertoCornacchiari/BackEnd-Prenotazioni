083c9238872b533fba835f8ea2812e2c059b817d
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-05-07 22:06:24 +0200

    New database

--- prenota.php
+++ prenota.php
@@ -1,32 +1,31 @@
 <?php
 
 include_once "config.php";
-require 'vendor/autoload.php';
-
-require 'vendor\Chillerlan\php-qrcode\src\QRCode.php';
 
 //Variabili valorizzate tramite POST
-$codice_fiscale = $_POST['codice'];
-$giorno = $_POST['dateDisponibili'];
-$sql_numero= "SELECT COUNT(*) AS n_prenotazioni FROM prenotazione WHERE DAY(prenotazione.giorno) = DAY('$giorno')";
 
-$n_prenotazioni = $pdo->query($sql_numero)->fetchAll()[0]["n_prenotazioni"];
+$rawdata = file_get_contents("php://input");
+// Let's say we got JSON
+$rawdata = json_decode($rawdata, true);
 
-if ($n_prenotazioni >= 5){
-    echo "Impossibile prenotare in questo giorno per numero di prenotazioni massimo raggiunto";
-    exit(0);
-}
+
+$codice_fiscale = $rawdata['codice'];
+//$giorno = new DateTime($rawdata['giorno']);
+$giorno = $rawdata['giorno'];
+$presidio = $rawdata['presidio'];
+
 
 function generateRandomString($length = 10) {
     return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
 }
 
 $codice = generateRandomString();
+echo json_encode($giorno);
 /*
 $filename = 'qrcode'.md5($codice.'|'.$errorCorrectionLevel.'|'.$matrixPointSize).'.png';
 
 QRcode::png($codice, $filename, $errorCorrectionLevel. $matrixPointSize, 2);
-*/
+
 $data = $codice;
 $options = new QROptions([
     'version'    => 5,
@@ -36,20 +35,22 @@ $options = new QROptions([
 
 $qrcode = new QRCode($options);
 $qrcode->render($data);
+*/
 
-
+/*
 //Query di inserimento preparata
-$sql = "INSERT INTO prenotazione VALUES(null, :codice_fiscale, :giorno, :codice)";
+$sql = "INSERT INTO prenotazione VALUES(null, :giorno, :codice_fiscale,  :codice, :annullata, :presidio)";
 
 //Inviamo la query al database che la tiene in pancia
 $stmt = $pdo->prepare($sql);
 
 //Inviamo i dati concreti che verranno messi al posto dei segnaposto(:...)
-$stmt->execute(['codice_fiscale'=>$codice_fiscale, 'giorno'=>$giorno, 'codice'=>$codice]);
+$stmt->execute(['giorno'=>$giorno, 'codice_fiscale'=>$codice_fiscale, 'codice'=>$codice, 'annullata'=>0, 'presidio'=>1]);
 
 //header('Location:Lista_prenotazioni.php');
 //exit(0);
 
 echo "<h2>Il tuo codice prenotazione è:$codice</h2>";
 
-echo "<img src='$filename' width='300px' height='300px'/>";
+//echo "<img src='$filename' width='300px' height='300px'/>";
+*/
--- sql/tamponi.sql
+++ sql/tamponi.sql
@@ -16,19 +16,126 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `prenotazioni`
+-- Table structure for table `account`
 --
 
-DROP TABLE IF EXISTS `prenotazioni`;
+DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `prenotazioni` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `codice_fiscale` varchar(16) NOT NULL,
-  `giorno` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeUtente` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nomeUtente` (`nomeUtente`),
+  UNIQUE KEY `password` (`password`),
+  KEY `id_admin` (`id_admin`),
+  CONSTRAINT `account_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `account`
+--
 
--- Dump completed on 2021-03-13 10:41:15
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prenotazione`
+--
+
+DROP TABLE IF EXISTS `prenotazione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prenotazione` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `giorno` date NOT NULL,
+  `codice_fiscale` varchar(20) NOT NULL,
+  `codice` varchar(40) NOT NULL,
+  `annullata` tinyint(1) NOT NULL,
+  `id_presidio` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_presidio` (`id_presidio`),
+  CONSTRAINT `prenotazione_ibfk_1` FOREIGN KEY (`id_presidio`) REFERENCES `presidio` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prenotazione`
+--
+
+LOCK TABLES `prenotazione` WRITE;
+/*!40000 ALTER TABLE `prenotazione` DISABLE KEYS */;
+INSERT INTO `prenotazione` VALUES (1,'2021-05-04','sgndfhdfhgsfdg','124213',0,1),(2,'2021-05-12','sdfghgsdfh','2316512',0,1),(4,'2021-05-12','aeerfhdefh','2456146',0,2),(5,'2021-05-11','afbdbdfghsg','152513412',1,1),(14,'2021-05-09','Roberto','BxMvp2UEb8',0,1),(15,'2021-05-09','Roberto','wltDyoW67V',0,1);
+/*!40000 ALTER TABLE `prenotazione` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `presidio`
+--
+
+DROP TABLE IF EXISTS `presidio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `presidio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `value` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `presidio`
+--
+
+LOCK TABLES `presidio` WRITE;
+/*!40000 ALTER TABLE `presidio` DISABLE KEYS */;
+INSERT INTO `presidio` VALUES (1,'Brescia Fiere','BresciaFiere'),(2,'Centro Congressi','CentroCongressi');
+/*!40000 ALTER TABLE `presidio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tampone`
+--
+
+DROP TABLE IF EXISTS `tampone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tampone` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `esito` varchar(20) DEFAULT NULL,
+  `id_sanitario` int(11) DEFAULT NULL,
+  `id_prenotazione` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_sanitario` (`id_sanitario`),
+  KEY `id_prenotazione` (`id_prenotazione`),
+  CONSTRAINT `tampone_ibfk_1` FOREIGN KEY (`id_sanitario`) REFERENCES `account` (`id`),
+  CONSTRAINT `tampone_ibfk_2` FOREIGN KEY (`id_prenotazione`) REFERENCES `prenotazione` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tampone`
+--
+
+LOCK TABLES `tampone` WRITE;
+/*!40000 ALTER TABLE `tampone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tampone` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-07 22:04:50

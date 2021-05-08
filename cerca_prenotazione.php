a23477bdc730b13d343251f4343f30fd09a78346
Roberto_Cornacchiari <56387052+RobertoCornacchiari@users.noreply.github.com>
2021-05-08 17:16:27 +0200

    Aggiunta la possibilità di controllare la prenotazione

--- cerca_prenotazione.php
+++ cerca_prenotazione.php
@@ -1,34 +1,45 @@
 <?php
 
-include_once 'config.php';
+include_once "config.php";
 
 //Variabili valorizzate tramite POST
-$codice_fiscale = $_POST['codiceFiscale'];
-$codice = $_POST['code'];
+
+$rawdata = file_get_contents("php://input");
+// Let's say we got JSON
+$rawdata = json_decode($rawdata, true);
+
+
+$codice_fiscale = $rawdata['codiceFiscale'];
+$codice = $rawdata['codice'];
 
 //Inviamo la query al database che la tiene in pancia
-$stmt = $pdo->query("SELECT giorno, annullata FROM prenotazioni WHERE prenotazioni.codice_fiscale = $codice_fiscale
-AND prenotazioni.codice = '$codice'");
+$stmt = $pdo->query("SELECT giorno, annullata, codice FROM prenotazione WHERE prenotazione.codice_fiscale = '$codice_fiscale'
+AND prenotazione.codice = '$codice'");
 
 //Inviamo i dati concreti che verranno messi al posto dei segnaposto(:...)
+$valori = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if ($stmt->rowCount() == 0){
-    echo "<h2>Valori inseriti non trovati</h2>";
+
+if ($valori == null){
+    echo json_encode("Errore");
     exit(0);
 }
 
-$valori = $stmt->fetch(PDO::FETCH_ASSOC);
+$valori = $valori[0];
+
 if ($valori['annullata'] == 0){
     $risultato = $valori['giorno'];
-
+    echo json_encode($valori);
+    /*
     echo "<h2>Sei prenotato per il giorno: $risultato</h2>";
     echo "<form action='annulla_prenotazione.php' method='post'>
          <input type='submit' value='Annulla la prenotazione'>
          <input type='hidden' name='code' value='$codice'/>
          <input type='hidden' name='codiceFiscale' value='$codice_fiscale'/>
     </form>";
+    */
 }
 else {
-    echo "<h2>La tua prenotazione è stata annullata; eseguirne una nuova</h2>";
+    echo json_encode("Annullata");
 }
 

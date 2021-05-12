af83b0a8efc8b2a2415d25eb5e841f7b4a6fc413
Roberto_Cornacchiari <roberto.cornacchiari@virgilio.it>
2021-05-12 12:45:15 +0200

    Prenotazione tra 2 date

--- prenotazioni_tra_date.php
+++ prenotazioni_tra_date.php
@@ -1,15 +1,13 @@
 <?php
 
 include_once 'config.php';
-require 'vendor/autoload.php';
-use League\Plates\Engine;
 
-//Variabili valorizzate tramite POST
-$giornoIniziale = $_POST['giornoIniziale'];
-$giornoFinale = $_POST['giornoFinale'];
+$presidio = $_GET['presidio'];
+$giornoIniziale = $_GET['giornoIniziale'];
+$giornoFinale = $_GET['giornoFinale'];
 
 //Inviamo la query al database che la tiene in pancia
-$stmt = $pdo->query("
+$stmt = $pdo->query(" SELECT tabella.gen_date as data, tabella.quanti FROM (
 SELECT data.gen_date, prenotazioni.quanti
 FROM (select * from
 (select adddate('1970-01-01',t4*10000 + t3*1000 + t2*100 + t1*10 + t0) gen_date from
@@ -19,31 +17,20 @@ FROM (select * from
  (select 0 t3 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
  (select 0 t4 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
 where gen_date BETWEEN '$giornoIniziale' AND '$giornoFinale') as data
-LEFT JOIN (SELECT prenotazioni.giorno, COUNT(*) as quanti FROM prenotazioni
-WHERE prenotazioni.giorno BETWEEN '$giornoIniziale' AND '$giornoFinale'
-AND prenotazioni.annullata = 0
-GROUP BY prenotazioni.giorno
-ORDER BY prenotazioni.giorno) as prenotazioni
-ON data.gen_Date = prenotazioni.giorno;
+LEFT JOIN (SELECT prenotazione.giorno, COUNT(*) as quanti FROM prenotazione, presidio
+WHERE prenotazione.giorno BETWEEN '$giornoIniziale' AND '$giornoFinale'
+AND prenotazione.id_presidio = presidio.id
+AND presidio.value = '$presidio'
+AND prenotazione.annullata = 0
+GROUP BY prenotazione.giorno
+ORDER BY prenotazione.giorno) as prenotazioni
+ON data.gen_Date = prenotazioni.giorno) as tabella
+
 ");
-
-
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$templates = new Engine('./view', 'tpl');
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
 
-echo $templates->render('prenotazioni_tra_date', ['result' => $result]);
-//header('Location:Lista_prenotazioni.php');
-//exit(0);
-
-//generatore di date
-/*
-(select * from
-(select adddate('1970-01-01',t4*10000 + t3*1000 + t2*100 + t1*10 + t0) gen_date from
-(select 0 t0 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
- (select 0 t1 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t1,
- (select 0 t2 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t2,
- (select 0 t3 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t3,
- (select 0 t4 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t4) v
-where gen_date BETWEEN '$giornoIniziale' AND '$giornoFinale') as date
-*/
+echo json_encode($result);
